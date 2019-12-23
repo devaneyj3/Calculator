a195a1c434c3556ae7235e32c4a1dc8ff2972dbe
Jordan Devaney <jordandevaney28@gmail.com>
2019-12-23 12:32:31 -0500

    handle if you press a number

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 //jshint esversion:6
-const calculation = document.querySelector('.calculation');
 const output = document.querySelector('.output');
-
 let buffer = '0';
+let previousOperator;
+let runningTotal = 0;
 
-let operator;
-
-calculation.addEventListener('click', (event) => {
+document.querySelector('.calculation').addEventListener('click', function(event) {
     buttonClick(event.target.textContent);
-    
 });
 
-function buttonClick( value) {
+function buttonClick(value) {
     if ( isNaN(parseInt(value))) {
-        //handle symbol button
-        handleSymbol( value);
-        }
+        handleSymbol(value);
+    }
     else {
-        //handle number button when pressed
         handleNumber( value);
     }
 }
@@ -25,61 +20,12 @@ function buttonClick( value) {
 function handleNumber( value) {
     if( buffer === '0') {
         buffer = value;
+        output.textContent = value;
     }
     else {
+        console.log('here');
         buffer += value;
+        console.log("Running total inside HandleNumber function is ", buffer);
         output.textContent = buffer;
     }
-}
-
-function handleSymbol( value) {
-    resetBuffer();
-    operator = value;
-    switch (operator){
-        case 'C':
-            buffer = "0";
-            output.textContent = buffer;
-            break;
-        case '/':
-            divide();
-            console.log("You are dividing");
-            break;
-        case 'X':
-            multiply();
-            console.log("You are multiplying");
-            break;
-        case '-':
-            subtract();
-            console.log("You are subtracting");
-            break;
-        case '+':
-            outputNum = add(1, 2);
-            console.log("You are adding = " + outputNum);
-            return outputNum;
-        case '=':
-            console.log("You are equaling " + outputNum);
-            output.textContent = outputNum;
-            break;
-    }
-}
-
-function add( a, b) {
-    return a + b;
-}
-
-function subtract(a, b) {
-    return a - b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function resetBuffer() {
-    buffer = ' ';
-    output.textContent = buffer;
 }
